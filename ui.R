07b954c1018627dfc21ed15ef03253df3d94e6b2
jdariasl <judaarlo@gmail.com>
2014-06-16 15:28:34 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,11 +8,11 @@ shinyUI(fluidPage(
   
   hr(),
   
-  # Sidebar with a slider input for the number of bins
+
   fluidRow(
     column(3,
            h3("Mixture of distributions"),
-                  
+            # RadioButton to select the type of distribution in the pdf exercise        
           radioButtons("DisType", h4("Type of Distribution"), 
                   list("Normal" = "norm",
                         "Log-normal" = "lnorm"),
@@ -22,7 +22,7 @@ shinyUI(fluidPage(
                   list("1" = "M1", 
                     "2" = "M2", "3" = "M3"),
                  selected = "M1"),
-                 
+            # Aditional panels depending on the number of mixture components       
           conditionalPanel(
                     condition = "input.Nmixtures == 'M1'",
                     numericInput("Mean1", 
@@ -50,25 +50,26 @@ shinyUI(fluidPage(
                     numericInput("Mean3", 
                                   label = h5("Mean component  3"), 
                                   value = 5)),
+            # Sidebar with a slider input for the number of samples
           sliderInput("n",
                     label = h4("Number of samples"), 
                     min = 10,
                     max = 200,
                     value = 100),
-     
+            # Sidebar with a slider input for the number of bins
           sliderInput("bins",
                     label = h4("Number of histogram's bins"), 
                     min = 1,
                     max = 50,
                     value = 30),
-           
+             # Sidebar with a slider input for the bandwidth
           sliderInput("h",
                     label = h4("Bandwidth Kernel estimatior"), 
                     min = 0.1,
                     max = 2,
                     value = 0.5)
     ),
-    
+     # Show plots in different tabs of the histogram and the kernel density function
     column(8, offset = 1,
            tabsetPanel(
              tabPanel("Histogram", plotOutput("distPlot")), 
@@ -81,20 +82,20 @@ shinyUI(fluidPage(
   fluidRow(
     column(3,
            h2("Kernel Regressor"),
-           
+             # Sidebar with a slider input for the number of samples in the regression exercise
            sliderInput("n2",
                        label = h4("Number of samples"), 
                        min = 10,
                        max = 200,
                        value = 100),
-           
+             # Sidebar with a slider input for the bandwith in the regression excercise
            sliderInput("h2",
                        label = h4("Bandwidth Kernel estimatior"), 
                        min = 0.1,
                        max = 2,
                        value = 0.5)
     ),
-    # Show a plot of the generated distribution
+    # Show plots in different tabs of the original data and the output of the Nadaraya-Watson regressor
     column(8, offset = 1,
            tabsetPanel(
              tabPanel("Original data", plotOutput("distPlot3")), 
